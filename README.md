da4ade3a3a2baa7a64eb4ee7f880db5d9397f6ef
unknown <tthpmx@TUTTHLT8VXT562.thomsonuk.dnsroot.biz>
2016-06-13 16:42:42 +0100

    added testing and babel compilation

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # NumberConverter
-Quick test to convert numeric values to English phrasing
+Quick test to convert numeric values to English phrasing on the command line.
+This is using mocha and chai for basic test cases, and babel for ES6 sugar,
+but otherwise it simply wraps the number-to-words package.
+
+Execution is done via npm scripts on the command line.
+
+## Install
+
+1. Clone this repo
+2. Install dependencies:
+  `npm install`
+3. Run tests (Mocha/Chai):
+  `npm test`
+
+## Usage
+
+Command line:
+```bash
+$ npm run convert [number]
+```
+...where [number] represents any number, e.g.:
+```bash
+$ npm run convert 123
+
+one hundred twenty-three
+```
--- test/converter-spec.js
+++ test/converter-spec.js
@@ -1,16 +1,24 @@
-import convert from '../converter.js'
+import convert from '../src/converter.js'
 import { expect } from 'chai'
 
 describe('converter', () => {
   it('converts digits under 20', () => {
-    const converted = [0, 1, 2, 4, 6, 8, 10, 13, 15, 19].map(n => convert(n))
-    expect(converted).to.equal([
-      'zero',
-      'one',
-      'two',
-      'three',
-      'four',
-      'five'
-    ])
+    expect(convert(0)).to.equal('zero')
+    expect(convert(1)).to.equal('one')
+    expect(convert(10)).to.equal('ten')
+    expect(convert(19)).to.equal('nineteen')
+  })
+
+  it('converts digits between 20 and 99', () => {
+    expect(convert(1)).to.equal('one')
+    expect(convert(19)).to.equal('nineteen')
+    expect(convert(8)).to.equal('eight')
+
+  })
+
+  it('converts digits between 100 and 999', () => {
+    expect(convert(100)).to.equal('one hundred')
+    expect(convert(368)).to.equal('three hundred sixty-eight')
+    expect(convert(999)).to.equal('nine hundred ninety-nine')
   })
 })
